5c528bcf66146b6a40a8a30ddfafa67bc521cde5
TetraAnalytics <130104657+TetraAnalytics@users.noreply.github.com>
2023-04-16 19:50:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+from Classes import Mortgage
+# from Read_write_csv import load_mortgage
+# from Read_write_csv import print_mortgage_results
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pandas as pd
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Mortgages:
+    def __init__(self, mortgage_term, loan_id, balance, note_rate):
+        self.mortgage_term = mortgage_term
+        self.loan_id = loan_id
+        self.balance = balance
+        self.note_rate = note_rate
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print("Balance = ", self.balance)
+        print("loan id = ", self.loan_id)
+
+        return
+
+
+def main():
+
+    csv_file_mortgage_attributes = '/Users/michaelaneiro/Downloads/mortgage_attributesv2.csv'  # MacBook
+    df = pd.read_csv(csv_file_mortgage_attributes)
+    # rows = len(df)
+    print("Print list:")
+    mortgage_list = df.iloc[1].tolist()
+    for i in range(0, len(mortgage_list)):
+        print(mortgage_list[i])
+    print("Create m")
+    m = Mortgages(mortgage_list[0], mortgage_list[1], mortgage_list[2], mortgage_list[3])
+    # m = Mortgages('30-year', 'ABC123', 300000.00, 0.045)
+    # print('mortgage_list: ',mortgage_list[0], " ", mortgage_list[1], " ", mortgage_list[2], " ", mortgage_list[3])
+    print("Print mortgage list: ", mortgage_list)
+
+    # my_objects = []
+    # obj = pd.read_csv(csv_file_mortgage_attributes)
+    # my_objects.append(obj)
+    # print(my_objects)
+
+    print("printing the dataframe:")
+    for row in range(len(df)):
+        print("df.loc[row].atloan_id = ", df.loc[row].at["loan_id"])  # or using df.
+        print("df.iloc[row] = ", df.iloc[row])
+
+main()
